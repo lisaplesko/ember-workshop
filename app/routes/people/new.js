1af9958c2fbef7abd2ad9fe56f6c3a461f7997cc
Lisa Plesko <plesko.l@gmail.com>
2014-06-16 15:47:23 -0400

    Edit/Destroy components

--- app/routes/people/new.js
+++ app/routes/people/new.js
@@ -11,6 +11,24 @@ export default Ember.Route.extend({
       this.controller.get('model').save().then(function() {
         routeContext.transitionTo('people');
       });
+    },
+    // willTransition: function(transition) {
+    //   var model = this.get('controller.model');
+    //   if (model.get('isDirty') && !confirm('You have unsaved changes. They will be lost if you continue!')) {
+    //   // isDirty - has anything changed?
+    //     transition.abort();
+    //   }
+  },
+  deactivate: function() {
+    // called ANYTIME you leave this route
+    // NOT called if you refer to a child route
+    var model = this.get('controller.model');
+    model.rollback();
+    if (model.get('isNew')) {
+      model.deleteRecord();
     }
   }
 });
+
+// We go to create a new record, decide we dont' want to create it.
+// Want to detect whether the record has been persisted or not.
